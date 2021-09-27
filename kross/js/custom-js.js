b58d8f8e1b18857d2361a3396ae16f903a1d47bd
Probir Sarkar <81101648+probir-sarkar@users.noreply.github.com>
2021-09-27 20:00:53 +0530

    collapse

---
+++ kross/js/custom-js.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('[data-toggle="collapse"]').click(function () {
+    $(this).toggleClass("active");
+    if ($(this).hasClass("active")) {
+      $(this).text("Less");
+    } else {
+      $(this).text("More");
+    }
+  });
+
+  // document ready
+});
