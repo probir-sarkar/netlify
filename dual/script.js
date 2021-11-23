5f01cee58495a7fc13b6bb8dde53a6bb75f0a8a1
Probir Sarkar <81101648+probir-sarkar@users.noreply.github.com>
2021-11-23 10:31:12 +0530

    d

--- dual/script.js
+++ dual/script.js
@@ -1,27 +1,37 @@
-//Session Storage
+/*Session Storage*/
 let playerOneCount = JSON.parse(sessionStorage.getItem("playerOneCount"));
 let playerTwoCount = JSON.parse(sessionStorage.getItem("playerTwoCount"));
 let drawCount = JSON.parse(sessionStorage.getItem("drawCount"));
-// End of Session Storage
-let clicked = false;
-//Audio file
+/*End of Session Storage*/
+
+/*Audio file*/
 let human = new Audio("human.wav");
 let robot = new Audio("robot.wav");
 robot.load();
 human.load();
-//End of audio file
-const playBtn = document
-  .querySelectorAll("button.playBtn");
-const disableBtn = () => {
-    playBtn.forEach((elem) => (elem.disabled = true));
-};
-const overlay = (result) => {
-  document.getElementById("result").innerHTML = result;
-  document.getElementById("overlay").style.visibility = "visible";
-  disableBtn();
-};
+/*End of audio file*/
+
+/*Player Determination*/
+
+// Notification Button Clicked
+let clicked = false;
+function typeX() {
+  if (clicked == false) {
+    player = "X";
+  }
+}
+function typeO() {
+  if (clicked == false) {
+    player = "O";
+  }
+}
+//Onload Default player Determinitation
 let player = "X";
-const pass = () => {
+
+/* End of Player Determination */
+
+/* Passing Condition */
+const checkResult = () => {
   ck = idArrayInnerHtml;
   let [a, b, c, d, e, f, g, h, i] = [
     ck[0],
@@ -71,7 +81,22 @@ const pass = () => {
     }
   }
 };
-const x = (btn) => {
+/* End Of Passing Condition*/
+
+
+
+const playBtn = document.querySelectorAll("button.playBtn");
+const disableBtn = () => {
+  playBtn.forEach((elem) => (elem.disabled = true));
+};
+
+const overlay = (result) => {
+  document.getElementById("result").innerHTML = result;
+  document.getElementById("overlay").style.visibility = "visible";
+  disableBtn();
+};
+
+const btnClicked = (btn) => {
   count--;
   colorChange(btn);
   document.getElementById("result").innerHTML = "Playing....";
@@ -81,7 +106,7 @@ const x = (btn) => {
   idArrayInnerHtml.splice(position, 1, player);
   btn.innerHTML = player;
   xy();
-  pass();
+  checkResult();
   clicked = true;
 };
 const colorChange = (btn) => {
@@ -104,40 +129,10 @@ const xy = () => {
     player = "X";
   }
 };
-let btn1 = document.querySelector("#btn1");
-let btn2 = document.querySelector("#btn2");
-let btn3 = document.querySelector("#btn3");
-let btn4 = document.querySelector("#btn4");
-let btn5 = document.querySelector("#btn5");
-let btn6 = document.querySelector("#btn6");
-let btn7 = document.querySelector("#btn7");
-let btn8 = document.querySelector("#btn8");
-let btn9 = document.querySelector("#btn9");
 
 let count = 9;
 
-let idArrayInnerHtml = [
-  btn1.innerHTML,
-  btn2.innerHTML,
-  btn3.innerHTML,
-  btn4.innerHTML,
-  btn5.innerHTML,
-  btn6.innerHTML,
-  btn7.innerHTML,
-  btn8.innerHTML,
-  btn9.innerHTML,
-];
-let idArrayInnerHtmlBackup = idArrayInnerHtml;
 
-btn1.onclick = () => x(btn1);
-btn2.onclick = () => x(btn2);
-btn3.onclick = () => x(btn3);
-btn4.onclick = () => x(btn4);
-btn5.onclick = () => x(btn5);
-btn6.onclick = () => x(btn6);
-btn7.onclick = () => x(btn7);
-btn8.onclick = () => x(btn8);
-btn9.onclick = () => x(btn9);
 
 let refresh = document.querySelector("#refresh");
 let reset = document.querySelector("#reset");
@@ -159,16 +154,48 @@ reset.onclick = () => {
   sessionStorage.clear();
   location.reload(true);
 };
+/* Fetch Game Data*/
+let idArrayInnerHtml = [];
+for (i = 1; i <= 9; i++) {
+  eval("let btn" + i + "= document.querySelector(`#btn${i}`)");
+  eval("idArrayInnerHtml.push(btn" + i + ".innerHTML)");
+  eval("btn"+i+".onclick = () => btnClicked(btn"+i+")");
+}
+/* Backup Code*/
+/*
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+let btn6 = document.querySelector("#btn6");
+let btn7 = document.querySelector("#btn7");
+let btn8 = document.querySelector("#btn8");
+let btn9 = document.querySelector("#btn9");
+*/
+//Save Button Data in array then change is as button clicked, It will help to determine result
+/*
+let idArrayInnerHtml = [
+  btn1.innerHTML,
+  btn2.innerHTML,
+  btn3.innerHTML,
+  btn4.innerHTML,
+  btn5.innerHTML,
+  btn6.innerHTML,
+  btn7.innerHTML,
+  btn8.innerHTML,
+  btn9.innerHTML,
+];*/
+/*End of Fetch Game Data */
 
-// Notification Buttons
-function typeX() {
-  if (clicked == false) {
-    player = "X";
-  }
-
-};
-function typeO() {
-  if (clicked == false) {
-    player = "O";
-  }
-}''
+/*
+btn1.onclick = () => btnClicked(btn1);
+btn2.onclick = () => btnClicked(btn2);
+btn3.onclick = () => btnClicked(btn3);
+btn4.onclick = () => btnClicked(btn4);
+btn5.onclick = () => btnClicked(btn5);
+btn6.onclick = () => btnClicked(btn6);
+btn7.onclick = () => btnClicked(btn7);
+btn8.onclick = () => btnClicked(btn8);
+btn9.onclick = () => btnClicked(btn9);
+*/
